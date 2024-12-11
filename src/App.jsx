9f0b3b93cf598895106b8ad9db13a8ffbf2c2609
Abhi <abhi2bari929@gmail.com>
2024-12-11 11:56:17 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Contact from './pages/contact';
+import Contact from './pages/Contact';
 import Project from './pages/Project';
 
 function App() {
